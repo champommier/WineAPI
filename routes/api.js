c0f3d5116414a7c5c25cfb670225cf69570fd9f8
Champommier <champommier@gmail.com>
2016-07-09 23:00:57 +0200

    Add cellar model

--- routes/api.js
+++ routes/api.js
@@ -4,10 +4,14 @@ var router = express.Router();
 
 // Models
 var Wine = require('../models/wine');
+var Cellar = require('../models/cellar');
 
 // Routes
 Wine.methods(['get', 'put', 'post', 'delete']);
 Wine.register(router, '/wines');
+
+Cellar.methods(['get', 'put', 'post', 'delete']);
+Cellar.register(router,'/cellars');
 
 // Return router
 module.exports = router;
