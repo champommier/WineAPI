639eb0ae7d6a43276020568906aed92f88062da7
Emmanuel Champommier <emmanuelchampommier@MacBook-Pro-de-Emmanuel.local>
2020-03-15 21:11:53 +0100

    clean

--- dist/index.d 2.ts
+++
@@ -1,4 +0,0 @@
-import { WineapiLoopbackApplication } from './application';
-import { ApplicationConfig } from '@loopback/core';
-export { WineapiLoopbackApplication };
-export declare function main(options?: ApplicationConfig): Promise<WineapiLoopbackApplication>;
--- dist/repositories/wine.repository.d 2.ts
+++
@@ -1,6 +0,0 @@
-import { DefaultCrudRepository } from '@loopback/repository';
-import { Wine, WineRelations } from '../models';
-import { DatabaseDataSource } from '../datasources';
-export declare class WineRepository extends DefaultCrudRepository<Wine, typeof Wine.prototype.identifier, WineRelations> {
-    constructor(dataSource: DatabaseDataSource);
-}
