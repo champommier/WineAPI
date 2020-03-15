39859563c279b966850a07def46dd35c0d724e69
Emmanuel Champommier <emmanuelchampommier@MacBook-Pro-de-Emmanuel.local>
2020-03-15 21:42:10 +0100

    MONGO_PORT

--- src/application.ts
+++ src/application.ts
@@ -9,12 +9,21 @@ import {RestApplication} from '@loopback/rest';
 import {ServiceMixin} from '@loopback/service-proxy';
 import path from 'path';
 import {MySequence} from './sequence';
+import { threadId } from 'worker_threads';
+import { DatabaseDataSource } from './datasources';
 
 export class WineapiLoopbackApplication extends BootMixin(
   ServiceMixin(RepositoryMixin(RestApplication)),
 ) {
   constructor(options: ApplicationConfig = {}) {
     super(options);
+
+    const db_port = process.env.MONGO_PORT || 27017;
+    this.bind('datasources.config.db').to({
+      port: db_port,
+    });
+
+    this.bind('datasource.DatabaseDataSource').toClass(DatabaseDataSource);
 
     // Set up the custom sequence
     this.sequence(MySequence);
