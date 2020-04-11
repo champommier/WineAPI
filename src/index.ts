0de3148645777f27e904cc539eacb81dcf84a41c
Emmanuel Champommier <emmanuelchampommier@MacBook-Pro-de-Emmanuel.local>
2020-04-12 00:32:00 +0200

    demo

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,6 @@ export async function main(options: ApplicationConfig = {}) {
 
   const url = app.restServer.url;
   console.log(`Server is running at ${url}`);
-  console.log('-------');
   console.log(`Try ${url}/ping to check`);
 
   return app;
